aef6ff17fd00d2c557e18031d9413c16d5c91910
Matias Bubello <matt.bubello@gmail.com>
2024-03-20 16:29:09 -0300

    h2 styles

--- src/components/H2.tsx
+++ src/components/H2.tsx
@@ -3,7 +3,14 @@ type Props = {
 };
 
 function H2({ text }: Props) {
-  return <h2 className={"mb-5 text-3xl text-center"}>{text}</h2>;
+  return (
+    <h2
+      className={
+        "mb-5 text-3xl text-center bg-gradient-to-r from-primary to-tertiary text-transparent bg-clip-text uppercase"
+      }>
+      {text}
+    </h2>
+  );
 }
 
 export default H2;
